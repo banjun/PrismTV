233b5d2bf2ddd8abfaed1066a99494c5efe1c28f
banjun <banjun@gmail.com>
2019-04-07 14:39:27 +0900

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :osx, '10.13'
+use_frameworks!
+
+target 'PrismTV' do
+  pod '※ikemen'
+  pod 'NorthLayout'
+  pod 'Kingfisher'
+end
---
+++ Pods/Target Support Files/Pods-PrismTV/Pods-PrismTV-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrismTV : NSObject
+@end
+@implementation PodsDummy_Pods_PrismTV
+@end
